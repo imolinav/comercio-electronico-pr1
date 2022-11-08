c82886b56bdc8f2ff212079a6b5bb8ecf4f0230a
imolinav <iamovaz@gmail.com>
2022-11-08 20:54:28 +0100

    refactor: remove console.log from scripts.js

--- pages/scripts.js
+++ pages/scripts.js
@@ -2,7 +2,6 @@ export function updateStyles(stylesRoute, rootLevel) {
     let linkElement = document.createElement('link');
     linkElement.rel = 'stylesheet';
     let hrefLink = `${rootLevel}/${stylesRoute}/${stylesRoute}.styles.css`;
-    console.log(hrefLink);
     linkElement.href = hrefLink;
     document.head.appendChild(linkElement);
 }
